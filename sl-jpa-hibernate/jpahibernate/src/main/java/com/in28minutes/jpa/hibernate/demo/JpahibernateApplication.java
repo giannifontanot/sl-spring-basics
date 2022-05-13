cd6e2447074b59b16c55b73637a733266188c3fa
Gianni Fontanot <giannifontanot@gmail.com>
2022-05-13 17:43:32 -0500

    First Version

---
+++ sl-jpa-hibernate/jpahibernate/src/main/java/com/in28minutes/jpa/hibernate/demo/JpahibernateApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpahibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpahibernateApplication.class, args);
+    }
+
+}
---
+++ sl-jpa-hibernate/jpahibernate/src/test/java/com/in28minutes/jpa/hibernate/demo/JpahibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpahibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
